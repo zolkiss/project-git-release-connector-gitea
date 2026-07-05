a1099f73f6b90aabc95fadd25e494651372c4094
zolkiss <kisszoltan89@gmail.com>
2026-07-05 13:12:43 +0200

    refactor: Splitting project between two repositories

--- core/src/pgr/change_log/__init__.py
+++
@@ -1,3 +0,0 @@
-from pgr.change_log.util import generate_change_chapters
-
-__all__ = ["generate_change_chapters"]
--- core/src/pgr/git/__init__.py
+++
@@ -1,4 +0,0 @@
-from pgr.git.data_classes import GitHashAndMsg
-from pgr.git.git_commander import GitCommander
-
-__all__ = ["GitCommander", "GitHashAndMsg"]
