56169a4a8db342d9a3e01f02bb0651a91291b87c
zolkiss <kisszoltan89@gmail.com>
2026-07-10 19:14:02 +0200

    test: Add unit tests (breaking under 80%)

--- test/project_git_release_connector_gitea/resolver_test.py
+++ test/project_git_release_connector_gitea/resolver_test.py
@@ -1,2 +1,8 @@
+from project_git_release.core import ReleaseConfig
+from project_git_release_connector_gitea import GiteaConnector
+
+
 def test_dummy_test():
+    release_config = ReleaseConfig(token="", url="dummy_url", owner="dummy_owner", repo="dummy_repo")
+    connector = GiteaConnector(release_config)
     assert True
